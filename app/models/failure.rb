d80f4838f25bf0b9a18db834d7c2289215a1f781
Graham Hadgraft <graham.hadgraft@gmail.com>
2021-02-28 21:29:38 +0000

    Fix attribute names

--- app/models/failure.rb
+++ app/models/failure.rb
@@ -3,7 +3,7 @@ require 'csv'
 class Failure < ApplicationRecord
 
   def self.to_csv
-    attributes = %w{repo_name, build_number, branch, username, circle_job, test_name test_file line_number failure}
+    attributes = %w{repo_name build_number branch username circle_job test_name test_file line_number failure}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
